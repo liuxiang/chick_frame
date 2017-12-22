84220f983278fe4dcc5568f46f05a2add5e471b7
liuxiang <xiang.liu@tongdu.cn>
2017-12-22 18:59:13 +0800

    增加编译war包支持(用于外置tomcat运行)

--- biz/service/src/main/java/me/liuxiang/chick/AppApplication.java
+++ biz/service/src/main/java/me/liuxiang/chick/AppApplication.java
@@ -2,6 +2,7 @@ package me.liuxiang.chick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ComponentScan(basePackages = "me.liuxiang")
 @ImportResource(locations = {"classpath:app.xml"})
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
